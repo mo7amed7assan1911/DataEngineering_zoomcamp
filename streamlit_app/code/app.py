72e38072fc1d4319be97850db6362ec5ee3e6cd3
Mohamed Hassan <mo7amed7assan1911@gmail.com>
2024-01-21 17:12:56 +0000

    Pending changes exported from your codespace

---
+++ streamlit_app/code/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import sys
+
+
+args = sys.argv
+
+st.write(f'''
+         # Hello world
+         
+        The arguments are: {args}
+         > Yes this is Quote.
+         ''')
