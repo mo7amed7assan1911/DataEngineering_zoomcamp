fd7da20b3613cf81968c1644c1476af007c13f81
Mohamed Hassan <mo7amed7assan1911@gmail.com>
2024-01-25 02:52:48 +0000

    Editing the environment

--- Contarization_of_postgreSQL/README.md
+++ Contarization_of_postgreSQL/README.md
@@ -1,37 +1,35 @@
-# Contarization_of_postgreSQL
+# ETL & Contarization of PostgreSQl env
 
+## Building the PostgreSQL Environment
 
-### First, you need to create external volume & network
-
-```bash
-docker create network pg-network
-docker create volume pg-volume
-```
-
-### Run the PostgreSQL environment by:
+#### You need to run this:
 
 ```bash
 docker-compose -f postgres_env.yml up -d
 ```
 
-### Load and save in the database the dataset of ny_taxi
+## Loading dataset then add it to the database
 
-##### First, creat a Image that makes the process
+#### You could run the ingest_data.py script to do that.
+> But, use good values in the arguments such as the user, password, host, port based on the running containers.
+
+> **NOTICE:** the host will be pgServer `server of the postgresql in our docker-compose file` if you run the script inside a container that on the pg-network.
+
+#### You need to run this:
+
 ```bash
-docker build -t taxi_data_image . 
-```
+trips_url="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-09.csv.gz"
 
-##### Then, create a container from this image and connect it to the same volume and network
+zones_url="https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv"
 
-```bash
-URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
-
-docker run -it --network=pg-network taxi_data_image \
+python ingest_data_homeWork.py \
     --user=root \
     --password=mypass \
-    --host=pgServer \
+    --host=localhost \
     --port=5432 \
-    --db=ny_taxi \
-    --table_name=yello_taxis \
-    --url=${URL}
+    --db=taxi_dataset \
+    --trips_table_name="taxi_trips" \
+    --zones_table_name="zones" \
+    --trips_data_url=${trips_url} \
+    --zones_data_url=${zones_url}
 ```
--- Contarization_of_postgreSQL/ingest_data.py
+++ Contarization_of_postgreSQL/ingest_data.py
@@ -2,6 +2,7 @@
 # Ingesting data from the source URL then save it in PostgreSQL database.
 import pandas as pd
 from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
 from time import time
 import argparse
 import os
@@ -12,43 +13,57 @@ def main(args):
     host = args.host
     port = args.port
     db   = args.db
-    table_name = args.table_name
-    csv_url    = args.url
-    csv_name   = 'output.csv'
+    trips_table_name = args.trips_table_name
+    zones_table_name = args.zones_table_name
+    zones_data_url    = args.zones_data_url
+    trips_data_url    = args.trips_data_url
     
-    # downloading the csv and name it 'output.csv'
-    os.system(f'wget {csv_url} -O {csv_name}')
+    trips_data_csv_name   = 'trips.csv'
+    zones_data_csv_name   = 'zones.csv'
+    
+    # downloading the tips csv and name it 'output.csv'
+    os.system(f'wget {trips_data_url} -O {trips_data_csv_name}')
+    os.system(f'wget {zones_data_url} -O {zones_data_csv_name}')
     
     print('Data Is downloaded 🥰')
-    df = pd.read_csv(csv_name, nrows=1, compression='gzip')
-    print('good dataframe')
+    df_trips = pd.read_csv(trips_data_csv_name, compression='gzip')
+    df_zones = pd.read_csv(zones_data_csv_name)    
     
+    print('good dataframes') 
+    
+    # connecting to the postgresql database.
+    # create new database if not exists with this name.
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-
-    engine.connect()
+    if not database_exists(engine.url):
+        create_database(engine.url)
+        
+    print(f'Connected to posgresql server | database: {db} 🥰')
     
-    print('Connected to posgresql server 🥰')
+    # Creating 2 tables schemas | no rows
+    df_trips.head(0).to_sql(con=engine, name=trips_table_name, if_exists='replace', index=False)
+    df_zones.head(0).to_sql(con=engine, name=zones_table_name, if_exists='replace', index=False)
 
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000, compression='gzip')
-    
-    # adding no line just making the schema of the database.
-    df.head(0).to_sql(con=engine, name=table_name, if_exists='replace', index=False)
-    
-    print('Created new schema 🥰')
+    # creating & inseting zones data.
+    df_zones.to_sql(con=engine, name=zones_table_name, if_exists='replace', index=False)
 
+    # We will not insert all data in one shot as it is has much records.
+    # making it iterable to send batches of records.
+    df_iter = pd.read_csv(trips_data_csv_name, iterator=True, chunksize=100000, compression='gzip')
+    
     for i, batch in enumerate(df_iter):
         
         t_start = time()
                 
-        batch.tpep_dropoff_datetime = pd.to_datetime(batch.tpep_dropoff_datetime)
-        batch.tpep_pickup_datetime  = pd.to_datetime(batch.tpep_pickup_datetime)
+        batch.lpep_dropoff_datetime = pd.to_datetime(batch.lpep_dropoff_datetime)
+        batch.lpep_pickup_datetime  = pd.to_datetime(batch.lpep_pickup_datetime)
 
-        batch.to_sql(con=engine, name=table_name, if_exists='append', index=False)
+        batch.to_sql(con=engine, name=trips_table_name, if_exists='append', index=False)
 
         t_end = time()
         print(f'Inserted batch {i + 1}, took {(t_end - t_start):.3f} sec 🥰')
-
-
+        
+    print('ETL is finished 🥰🤝')
+    
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='Ingest CSV to postgresql database')
@@ -59,23 +74,28 @@ if __name__ == '__main__':
     parser.add_argument('--host', help='host of the pg server')
     parser.add_argument('--port', help='port of pg server')
     parser.add_argument('--db', help='database name')
-    parser.add_argument('--table_name', help='table name')
-    parser.add_argument('--url', help='url of the csv file')
+    parser.add_argument('--trips_table_name', help='trips data table name')
+    parser.add_argument('--zones_table_name', help='zones data table name')
+    parser.add_argument('--trips_data_url', help='url of the green trips data as csv file')
+    parser.add_argument('--zones_data_url', help='url of the zones data as csv file')
     
     args = parser.parse_args()
     
     main(args=args)
 
 
-# # the host is pg_server if you run the script inside a container that on the pg-network.
+# the host is pg_server if you run the script inside a container that on the pg-network.
 
-# URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+# trips_url="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-09.csv.gz"
+# zones_url="https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv"
 
-# python ingest_data.py \
+# python ingest_data_homeWork.py \
 #     --user=root \
 #     --password=mypass \
 #     --host=localhost \
 #     --port=5432 \
-#     --db=ny_taxi \
-#     --table_name=yello_taxis \
-#     --url=${URL}
+#     --db=taxi_dataset \
+#     --trips_table_name="taxi_trips" \
+#     --zones_table_name="zones" \
+#     --trips_data_url=${trips_url} \
+#     --zones_data_url=${zones_url}
